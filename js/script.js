52789ac81fc22812dbe205822fe013180dd4adf2
TarasShyshko <tarr13579@gmail.com>
2023-04-21 18:02:10 +0300

    update

--- js/script.js
+++ js/script.js
@@ -4,4 +4,7 @@ $(document).ready(function () {
 
         $('.animated-icon1').toggleClass('open');
     });
+    document.getElementById("change-color-btn").addEventListener("click", function() {
+        document.getElementById("my-div").classList.toggle("my-class");
+    });
 });
